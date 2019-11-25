eb81d4be87f331c41c4877ba1ae08c1e4e9bf1b3
kevinnls <kevin.nl.samuel@gmail.com>
2019-11-26 00:29:01 +0530

    trying to loop and prevent repetition of word

--- knls-hangman.py
+++ knls-hangman.py
@@ -12,68 +12,94 @@ def dashify(dashes):
     for i in dashes:
         print(i ,end = " ")
     print ("\n")
-def hangman():
+def top():
     clear()
     print("welcome to hangman\n")
+    print(completed)
     dashify(dashes)
     print("clue: " + clue)
-def hangman_loss():
+def top_loss():
     clear()
     print("welcome to hangman\n")
     dashify(dashes)
     dashify(word)
     print("clue: " + clue)
 
-with open('dictionary.json') as dictx:
-   # word = listify(choice(json.load(dict)))
-    dict=json.load(dictx)
-    word, clue = choice(list(dict.items()))
+def hangman():
+    letters = []
+    global word
+    global clue
+    global dashes
+    global completed
+    chances = 6   
+    with open('dictionary.json') as dictx:
+    # word = listify(choice(json.load(dict)))
+        dict=json.load(dictx)
+    for x in completed:
+        if listify(x) == word:
+            word, clue = choice(list(dict.items()))
+        
+    completed.append(word)
     word = listify(word)
     dashes = listify("_" * len(word))
-    letters = []
-    chances = 6
-dictx.close()    
-
-hangman()
-print("\n")
-
-while dashes != word and chances>0:
     
-    print("used letters: " + str(letters))
-    print("chances left " + str(chances) +"/6")
-    guess = input("guess a letter: ")
+    dictx.close()    
+    
+    top()
+    print("\n")
+
+    while dashes != word and chances>0:
+    
+        print("used letters: " + str(letters))
+        print("chances left " + str(chances) +"/6")
+        guess = input("guess a letter: ")
     
     ###tracking used letters###
-    if len(guess)>1 or guess == '' :
-        hangman()
-        print("\ntype one letter at a time please\n")
-        continue
-    elif guess in letters:
-        hangman()
-        print("\nthis letter has already been used\n")
-        continue
-    elif guess not in letters:
-        letters.append(guess)   
+        if len(guess)>1 or guess == '' :
+            top()
+            print("\ntype one letter at a time please\n")
+            continue
+        elif guess in letters:
+            top()
+            print("\nthis letter has already been used\n")
+            continue
+        elif guess not in letters:
+            letters.append(guess)   
     ###tracking used letters###
     
     ###check letter in word###
-    if guess in word:
-        n=-1
-        for i in word:
-            n+=1
-            if guess == i:
-                dashes[n] = word[n]
-        hangman()
-        print("\n")
+        if guess in word:
+            n=-1
+            for i in word:
+                n+=1
+                if guess == i:
+                    dashes[n] = word[n]
+            top()
+            print("\n")
          
-    elif guess not in word:
-        chances-=1
-        hangman()
-        print("\nthat letter is not in the word\n")
+        elif guess not in word:
+            chances-=1
+            top()
+            print("\nthat letter is not in the word\n")
     
-if chances>0:
+    if chances>0:
+        top()
+        print("\ncongrats! you have won!")
+    else:
+        top_loss()
+        print("\nsorry. you have lost.")
+dashes = ''
+clue = ''
+word = 'null'
+completed = ['null']
+while True:
     hangman()
-    print("\ncongrats! you have won!")
-else:
-    hangman_loss()
-    print("\nsorry. you have lost.")
+    
+    choice = input("wanna play again? y/n: ")
+    if choice == 'y':
+        hangman()     
+    elif choice == 'n':
+        print("thanks! bye bye!")
+        exit()
+    else:
+        print("invalid choice")         
